22d233b6dd022e679eabfba891945c531657ca26
Ondrej Capek <ocapek@gmail.com>
2025-10-21 20:56:20 +0200

    log page

--- src/server/main.py
+++ src/server/main.py
@@ -9,7 +9,7 @@ import pandas as pd
 selected_ids = []
 
 def root():
-    ui.sub_pages({'/': main, '/add_page': add_page })
+    ui.sub_pages({'/': main, '/add_page': add_page, '/log_page': log_page })
 
 def main():
     res = db.get_active_records()
@@ -21,7 +21,10 @@ def main():
         ui.label(str(len(res)))
         ui.label("Average: ")
         ui.label(f"{statistics.mean(res)}")
+    with ui.row():
         ui.link('Go to add page', '/add_page')
+        ui.link('Go to log page', '/log_page')
+
 
 async def add_page():
     logging.debug("Visit add page")
@@ -35,6 +38,13 @@ async def add_page():
         await ba.clicked()
         db.add_value(val.value)
 
+def log_page():
+    logging.debug("Visit log page")
+    ui.label('Log page').classes("title")
+    f = open("../../log/debug.log")
+    log = ui.log(max_lines=100).classes("w-screen")
+    log.push("\n".join(f.readlines()[-100:]))
+    ui.link('Go to main page', '/')
 
 data_change.set_logs()
 logging.info("Start application...")
